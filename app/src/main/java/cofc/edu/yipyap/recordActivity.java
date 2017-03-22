a35c9ed082b3f737a22d3d01de90d094105a83e4
keanejc <keanejc@g.cofc.edu>
2017-03-22 16:35:40 -0400

    3 activities done, still need to test

--- app/src/main/java/cofc/edu/yipyap/recordActivity.java
+++ app/src/main/java/cofc/edu/yipyap/recordActivity.java
@@ -1,19 +1,33 @@
 package cofc.edu.yipyap;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class recordActivity extends AppCompatActivity {
+    // Button Id's
+    /*
+Back Button back2
+    * */
+
+    Button backButton = (Button) findViewById(R.id.back2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
 
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(recordActivity.this, startActivity.class));
+            }
+        });
     }
 
 }
