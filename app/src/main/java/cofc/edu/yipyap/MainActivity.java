3ba234f84a641f79d536ab958a7de269923342dd
William B <blanchettwa@g.cofc.edu>
2017-04-20 19:55:27 -0400

    Edited MainActivity for easier code writing

--- app/src/main/java/cofc/edu/yipyap/MainActivity.java
+++ app/src/main/java/cofc/edu/yipyap/MainActivity.java
@@ -3,6 +3,9 @@ package cofc.edu.yipyap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//Recieves number of turns, PlayerList, and Topic from the config actifity. They are named
+//     as follors, respectively: "NumTurns","PlayerList","Topic"
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
