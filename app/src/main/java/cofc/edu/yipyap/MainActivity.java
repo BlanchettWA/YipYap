51ef5962a27106edae9807d81841e99d56d9a09f
William B <blanchettwa@g.cofc.edu>
2017-04-23 23:53:34 -0400

    Edited Game screen into AndroidMenefest and commented out game loop: Caused UI crash.

--- app/src/main/java/cofc/edu/yipyap/MainActivity.java
+++ app/src/main/java/cofc/edu/yipyap/MainActivity.java
@@ -18,22 +18,25 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // Variable initialization
+    //Placed outisde of the function so they can persist through the whole activity
+    TextView topic;
+    TextView playerTurn;
+    EditText enterWords;
+    Button home;
+    Button done;
+    TextView wordsPosted;
+    TextView roundsLeft;
+
+
+    int turnCount = 1;
+    String player;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
-        // Variable initialization
-        TextView topic;
-        TextView playerTurn;
-        EditText enterWords;
-        Button home;
-        Button done;
-        TextView wordsPosted;
-        TextView roundsLeft;
-
-        int turnCount = 1;
-        String player;
 
         topic = (TextView) findViewById(R.id.topicBox);
         playerTurn = (TextView) findViewById(R.id.playerTurn);
@@ -45,14 +48,28 @@ public class MainActivity extends AppCompatActivity {
 
 
         // retrieving bundle from configActivity
-        Bundle extras = getIntent().getExtras();
-        int numTurns = extras.getInt("NumTurns");
-        ArrayList<String> playerList = extras.getStringArrayList("PlayerList");
-        String gameTopic = extras.getString("Topic");
+
+        System.out.println("HEY I GOT THE INITIAL STUFF DONE");
+        System.out.println("________________________________");
+        Intent extras = getIntent();
+        System.out.println("HEY I CREATED AN INTENT");
+        System.out.println("________________________________");
+
+
+
+        int numTurns = extras.getIntExtra("NumTurns",5);
+        ArrayList<String> playerList = extras.getStringArrayListExtra("PlayerList");
+        String gameTopic = extras.getStringExtra("Topic");
+
+        System.out.println("HEY I SET SOME VALUES UP");
+        System.out.println("________________________________");
 
         topic.setText("Topic: " + gameTopic);
         roundsLeft.setText("Rounds Left: " + numTurns);
 
+        System.out.println("ABOUT TO GET INTO A LOOP");
+
+        /*
         // Game flow
         while(turnCount <= numTurns) {
             for(int i = 1; i < playerList.size(); i++) {
@@ -61,8 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
-
-
+*/
 
 
 
