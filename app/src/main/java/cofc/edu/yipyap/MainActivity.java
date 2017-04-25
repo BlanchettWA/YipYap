b7f45dd28cdf6eefd7c37476226ccf2e1d9a92c3
Luke Bradley <bradleyla@g.cofc.edu>
2017-04-25 01:14:35 -0400

    Finished most of the game screen; just need to test words ArrayList and connect it to resultsActivity.

--- app/src/main/java/cofc/edu/yipyap/MainActivity.java
+++ app/src/main/java/cofc/edu/yipyap/MainActivity.java
@@ -3,15 +3,19 @@ package cofc.edu.yipyap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
 import android.text.TextWatcher;
 import android.content.Intent;
+import android.view.KeyEvent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputType;
 import java.util.ArrayList;
+
 
 //Recieves number of turns, PlayerList, and Topic from the config actifity. They are named
 //     as follors, respectively: "NumTurns","PlayerList","Topic"
@@ -23,15 +27,17 @@ public class MainActivity extends AppCompatActivity {
     //Placed outisde of the function so they can persist through the whole activity
     TextView topic;
     TextView playerTurn;
-    EditText enterWords;
+    TextView enterWordsText;
     Button home;
     Button done;
     TextView wordsPosted;
     TextView roundsLeft;
 
 
-    int turnCount = 1;
+    int roundCount = 1;
+    int playerNum = 0;
     String player;
+    ArrayList<String> words = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,45 +46,69 @@ public class MainActivity extends AppCompatActivity {
 
         topic = (TextView) findViewById(R.id.topicBox);
         playerTurn = (TextView) findViewById(R.id.playerTurn);
-        enterWords = (EditText) findViewById(R.id.enterWords);
+        enterWordsText = (TextView) findViewById(R.id.enterWordsText);
         home = (Button) findViewById(R.id.home);
         done = (Button) findViewById(R.id.done);
-        wordsPosted = (TextView) findViewById(R.id.wordsPosted);
+        wordsPosted = (EditText) findViewById(R.id.wordsPosted);
         roundsLeft = (TextView) findViewById(R.id.roundsLeft);
 
 
         // retrieving bundle from configActivity
 
-        System.out.println("HEY I GOT THE INITIAL STUFF DONE");
-        System.out.println("________________________________");
         Intent extras = getIntent();
-        System.out.println("HEY I CREATED AN INTENT");
-        System.out.println("________________________________");
 
-
-
-        int numTurns = extras.getIntExtra("NumTurns",5);
-        ArrayList<String> playerList = extras.getStringArrayListExtra("PlayerList");
-        String gameTopic = extras.getStringExtra("Topic");
-
-        System.out.println("HEY I SET SOME VALUES UP");
-        System.out.println("________________________________");
+        roundCount = extras.getIntExtra("NumTurns",5);
+        final ArrayList<String> playerList = extras.getStringArrayListExtra("PlayerList");
+        final String gameTopic = extras.getStringExtra("Topic");
 
         topic.setText("Topic: " + gameTopic);
-        roundsLeft.setText("Rounds Left: " + numTurns);
+        roundsLeft.setText("Rounds Left: " + roundCount);
+        playerTurn.setText("Player's Turn: " + playerList.get(playerNum));
 
-        System.out.println("ABOUT TO GET INTO A LOOP");
-
-        /*
-        // Game flow
-        while(turnCount <= numTurns) {
-            for(int i = 1; i < playerList.size(); i++) {
-                player = playerList.get(i);
-                playerTurn.setText("Player Turn: " + player);
-
+        wordsPosted.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (roundCount == 0) {
+                    Toast.makeText(getApplicationContext(), "The jig is up! Click 'Done' to see your full story.", Toast.LENGTH_SHORT).show();
+                } else {
+                    String word = wordsPosted.getText().toString();
+                    if (word.length() > 0) {
+                        words.add(word.toString());
+                        playerNum++;
+                        if (playerNum < playerList.size()) {
+                            playerTurn.setText("Player's Turn: " + playerList.get(playerNum));
+                        } else {
+                            playerNum = 0;
+                            playerTurn.setText("Player's Turn: " + playerList.get(playerNum));
+                            roundCount--;
+                            roundsLeft.setText("Rounds Left: " + roundCount);
+                        }
+                        wordsPosted.setText("");
+                    }
+                }
+                return handled;
             }
-        }
-*/
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.startActivity(new Intent(MainActivity.this, startActivity.class));
+            }
+        });
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toResult = new Intent(MainActivity.this, recordActivity.class);
+                toResult.putExtra("NumRounds", roundCount);
+                toResult.putExtra("PlayerList",playerList);
+                toResult.putExtra("Topic", gameTopic);
+                toResult.putExtra("WordStory",words);
+                MainActivity.this.startActivity(toResult);
+            }
+        });
 
 
 
